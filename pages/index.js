d98f483062a385c1f41cec536be2fcb7d71e6cbe
Haerul Muttaqin <haerul.muttaqin@gmail.com>
2022-06-02 23:38:53 +0700

    change update

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,6 @@ export default function Home() {
                 <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css" />
             </Head>
             <div className="container-main">
-                <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet" />
-                <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css" />
                 <header>
                     <div className="container">
                         <nav className="navbar navbar-dark bg-transparenet">
